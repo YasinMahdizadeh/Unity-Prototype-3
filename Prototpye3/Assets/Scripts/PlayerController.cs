073e772de79337fabc430e30c24124560b072b7c
Yasin Mahdizadeh <yasin.mahdizade@gmail.com>
2020-05-09 15:12:49 +0430

    Session1 Done

---
+++ Prototpye3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRB;
+    public float jumpForce = 10f;
+    public float gravityModifier = 2;
+    public bool isOnGround = true;
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        playerRB.AddForce(Vector3.up * 1000);
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRB.AddForce(Vector3.up * jumpForce ,ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
+}
